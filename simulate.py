312123c6f0ef366d3b23a0c04c8979b5bd34c646
willzhong121 <williamzhong121@gmail.com>
2023-01-08 23:42:20 -0600

    added block

--- simulate.py
+++ simulate.py
@@ -1,8 +1,9 @@
 import pybullet as p
 import time
 physicsClient = p.connect(p.GUI)
+p.loadSDF("box.sdf")
 for i in range(999):
-    time.sleep(i)
+    time.sleep(1/60)
     p.stepSimulation()
     print(i)
 p.disconnect()
